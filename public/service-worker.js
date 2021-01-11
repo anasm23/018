2ce0184e21397c8a1823045ecd617828860ff663
Muhammad Anas <muhammadanas112@gmail.com>
2021-01-11 13:47:27 -0500

    minor changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,13 @@
 // Declare files to cache
 const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./index.js",
-    "./style.css",
+    "/index.html",
+    "/index.js",
+    "/style.css",
+    '/db.js',
+    'manifest.json',
 ];
 /////
-const CACHE_NAME = "static-cache-v1";
+const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = 'data-cache-v1'
 //////
 const iconSizes = ["192"];
@@ -15,9 +16,9 @@ const iconFiles = iconSizes.map(
 );
 /////
 // install
-self.addEventListener("install", evt =>{
+self.addEventListener("install", function (evt){
     evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)),
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)),
         console.log("success")
     );
     self.skipWaiting();
@@ -38,31 +39,7 @@ self.addEventListener('activate', evt => {
   );
   self.clients.claim();
 });
-self.addEventListener("install", (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-
-  self.skipWaiting();
-});
 //////
-self.addEventListener("activate", (evt) => {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
 //////
 self.addEventListener("fetch", (evt) => {
   if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
@@ -78,7 +55,7 @@ self.addEventListener("fetch", (evt) => {
 
               return response;
             })
-            .catch(() => {
+            .catch(err => {
               return cache.match(evt.request);
             });
         })
